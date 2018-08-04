3653111466321a1960959b6e86d249127f926d92
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2018-08-04 16:00:40 -0400

    db set

--- 2bamazoncustomer.js
+++ 2bamazoncustomer.js
@@ -54,9 +54,9 @@ function displayProducts() {
                 connection.query(query, { id: answer.IDnumber }, function (err, results) {
                     if (err) throw err;
                     console.log(results);
-            console.log(results[0].stock_quantity);
-           console.log(results[0].price);
-           console.log(answer.quantity);
+                    console.log(results[0].stock_quantity);
+                    console.log(results[0].price);
+                    console.log(answer.quantity);
                     if (results[0].stock_quantity <= answer.quantity) {
                         console.log("I am sorry, we do not have enough items in stock to fulfill your order. Please check again next week.");
                         connection.end();
@@ -65,9 +65,55 @@ function displayProducts() {
                         console.log(total);
                         console.log("We have " + results[0].stock_quantity + " in stock. We can fill your order. Your total is $" + total + " Please check your email for an invoice.");
                     }
-                    
+                    var newStock = {
+                        "results": results[0].stock_quantity,
+                        "quantity": answer.quantity,
+                        "answer": answer.IDnumber
+                    };
+                    return newStock; // make newStock an object that can be returned from this function
+                    // because to update database need ID and new quantity
                 });
             });
+    });
+}
+
+function updateProduct() {
+    var id = newStock.answer;
+    var item_quantity = newStock.results - newStock.quantity;
+    var query = connection.query(
+        "UPDATE products SET ? WHERE ?",
+        [
+            {
+                item_quantity: item_quantity
+            },
+            {
+                id: id
+            }
+        ],
+        function (error) {
+            if (error) throw err;
+
         });
-    }
-    //still need to add code that subtracts from DB and updates the total.
+}
+    /*still need to add code that subtracts from DB and updates the total.
+    
+     connection.query(
+            "UPDATE auctions SET ? WHERE ?",
+            [
+              {
+                highest_bid: answer.bid
+              },
+              {
+                id: chosenItem.id
+              }
+            ],
+            function(error) {
+              if (error) throw err;
+              console.log("Bid placed successfully!");
+              start();
+            }
+          );
+        }  */
+
+
+
